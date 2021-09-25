74c73986c53e568a09dac826432d2b4d09774ac5
buyolitsez <hamerfall2003@yandex.ru>
2021-09-25 19:16:08 +0300

    add calculating hash

---
+++ src/main/kotlin/Hash.kt
@@ -0,0 +1,9 @@
+fun getHash(key: String) : ULong {
+    val coefficient : ULong = 97.toULong()
+    val mod : ULong = 576460752303423619.toULong() // big prime module ~= 5e17
+    var hash : ULong = 0u
+    key.forEach {
+        hash = (hash * coefficient + it.code.toULong()) % mod
+    }
+    return hash
+}
