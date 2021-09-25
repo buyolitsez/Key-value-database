476a56bd9f7daf10dcb4f39a534a0db135b18887
buyolitsez <hamerfall2003@yandex.ru>
2021-09-25 19:15:57 +0300

    Prepare to main database functions

---
+++ src/main/kotlin/CommonVariables.kt
@@ -0,0 +1,19 @@
+/** Name of function and count of arguments*/
+val FUNCTIONS = mapOf(
+    "contains" to 1,
+    "get" to 1,
+    "set" to 2,
+    "remove" to 1,
+    "size" to 0,
+    "is-empty" to 0,
+    "clear" to 0,
+    "entries" to 0,
+    "keys" to 0,
+    "values" to 0
+)
+
+/** Separator in every file*/
+val SEPARATOR = '='
+
+/** Max size of file in bytes*/
+val MAX_RECORDS_FILE = 10
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
 fun main(args: Array<String>) {
-    TODO()
+    startDatabase()
+    startOperation(args)
 }
