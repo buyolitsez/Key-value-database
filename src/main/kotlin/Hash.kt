2eaa3645875c1ce6ee97c65050fa5226676d5815
buyolitsez <hamerfall2003@yandex.ru>
2021-09-25 22:53:59 +0300

    Change types from Long to ULong

--- src/main/kotlin/Hash.kt
+++ src/main/kotlin/Hash.kt
@@ -1,7 +1,7 @@
-fun getHash(key: String) : ULong {
-    val coefficient : ULong = 97.toULong()
-    val mod : ULong = 576460752303423619.toULong() // big prime module ~= 5e17
-    var hash : ULong = 0u
+fun getHash(key: String): ULong {
+    val coefficient: ULong = 97.toULong()
+    val mod: ULong = 576460752303423619.toULong() // big prime module ~= 5e17
+    var hash: ULong = 0u
     key.forEach {
         hash = (hash * coefficient + it.code.toULong()) % mod
     }
