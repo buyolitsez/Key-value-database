a3c81cc01f66bebe8e0e0512c0706524a4d798a2
buyolitsez <hamerfall2003@yandex.ru>
2021-09-25 22:45:13 +0300

    Change options and parameters

--- src/main/kotlin/CommonVariables.kt
+++ src/main/kotlin/CommonVariables.kt
@@ -1,5 +1,6 @@
 /** Name of function and count of arguments*/
 val FUNCTIONS = mapOf(
+    "exit" to 0,
     "contains" to 1,
     "get" to 1,
     "set" to 2,
@@ -7,13 +8,11 @@ val FUNCTIONS = mapOf(
     "size" to 0,
     "is-empty" to 0,
     "clear" to 0,
-    "entries" to 0,
-    "keys" to 0,
     "values" to 0
 )
 
 /** Separator in every file*/
 val SEPARATOR = '='
 
-/** Max size of file in bytes*/
-val MAX_RECORDS_FILE = 10
+/** The count of lines in each file less than [MAX_RECORDS_FILE]*/
+val MAX_RECORDS_FILE = 3
