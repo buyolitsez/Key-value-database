f65f8d913815a2abebd5bad9f75e9276f8c78734
buyolitsez <hamerfall2003@yandex.ru>
2021-09-28 16:32:43 +0300

    Add tests for GetOperation

--- src/test/kotlin/TestGetDBOperation.kt
+++ src/test/kotlin/TestGetDBOperation.kt
@@ -34,32 +34,31 @@ internal class TestGetDBOperation {
         for (i in 0..10) {
             localDB.set(i.toULong(), i.toString())
         }
-        System.setOut(standardOut)
         for (i in 0..20) {
             localDB.get(i.toULong())
         }
         assertEquals(
-            "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false", stream.toString().trim()
+            "0\n" +
+                    "1\n" +
+                    "2\n" +
+                    "3\n" +
+                    "4\n" +
+                    "5\n" +
+                    "6\n" +
+                    "7\n" +
+                    "8\n" +
+                    "9\n" +
+                    "10\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key", stream.toString().trim()
         )
     }
 
@@ -77,27 +76,27 @@ internal class TestGetDBOperation {
             localDB.get(i.toULong())
         }
         assertEquals(
-            "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "true\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false\n" +
-                    "false", stream.toString().trim()
+            "5\n" +
+                    "6\n" +
+                    "7\n" +
+                    "8\n" +
+                    "9\n" +
+                    "10\n" +
+                    "11\n" +
+                    "12\n" +
+                    "13\n" +
+                    "14\n" +
+                    "15\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key\n" +
+                    "No such key", stream.toString().trim()
         )
     }
 
