c82b8cc67eb3fc10c873efe0c629525d41b3943e
buyolitsez <hamerfall2003@yandex.ru>
2021-09-28 16:25:32 +0300

    Delete useless string

---
+++ src/test/kotlin/TestGetDBOperation.kt
@@ -0,0 +1,104 @@
+import kotlin.test.*
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+internal class TestGetDBOperation {
+    private val standardOut = System.out
+    private val stream = ByteArrayOutputStream()
+
+    @BeforeTest
+    fun setUp() {
+        System.setOut(PrintStream(stream))
+    }
+
+    @BeforeTest
+    fun setConstants() = run {
+        PATH_DATA_DIRECTORY = "testData/TestGetDBOperation/"
+        MAX_RECORDS_FILE = 5
+    }
+
+    @AfterTest
+    fun setAll() = run {
+        setDefaultValues()
+    }
+
+    @AfterTest
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+
+    @Test
+    fun testSet1To10AndThenCheck() {
+        val localDB = Database()
+        localDB.clear()
+        for (i in 0..10) {
+            localDB.set(i.toULong(), i.toString())
+        }
+        System.setOut(standardOut)
+        for (i in 0..20) {
+            localDB.get(i.toULong())
+        }
+        assertEquals(
+            "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false", stream.toString().trim()
+        )
+    }
+
+    @Test
+    fun testReset1To10() {
+        val localDB = Database()
+        localDB.clear()
+        for (i in 0..10) {
+            localDB.set(i.toULong(), i.toString())
+        }
+        for (i in 0..10) {
+            localDB.set(i.toULong(), (i + 5).toString())
+        }
+        for (i in 0..20) {
+            localDB.get(i.toULong())
+        }
+        assertEquals(
+            "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "true\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false\n" +
+                    "false", stream.toString().trim()
+        )
+    }
+
+}
