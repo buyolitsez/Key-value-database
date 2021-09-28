67c5c20c1c5709fd37b5c980cae6cc98df820605
buyolitsez <hamerfall2003@yandex.ru>
2021-09-28 21:25:17 +0300

    Correct number of functions

--- src/main/kotlin/CommonVariables.kt
+++ src/main/kotlin/CommonVariables.kt
@@ -1,10 +1,10 @@
 /** Name of function and count of arguments*/
 val FUNCTIONS = mapOf(
     "exit" to 0,
-    "contains" to 1,
+    "containsKey" to 1,
     "get" to 1,
     "set" to 2,
-    "remove" to 1,
+    "removeKey" to 1,
     "size" to 0,
     "is-empty" to 0,
     "clear" to 0,
